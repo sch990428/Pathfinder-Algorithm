a1616952fc12775632df1c68625e66b2b4512ac6
sch990428 <990428sch@naver.com>
2023-11-05 13:33:40 +0900

    타일 출력

---
+++ Maze.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Pathfinder_Algorithm {
+	internal class Maze {
+		const char TILE = '\u25a0';
+
+		public TileType[,] _tile;
+		public int _size;
+
+		public enum TileType {
+			Empty,
+			Wall,
+		}
+
+		public void init(int size) {
+			_tile = new TileType[size, size];
+			_size = size;
+
+			for (int y = 0; y < size; y++) {
+				for (int x = 0; x < size; x++) {
+					if (x == 0 || x == size - 1 || y == 0 || y == size - 1) {
+						_tile[y, x] = TileType.Wall;
+					} else {
+						_tile[y,x] = TileType.Empty;
+					}
+				}
+			}
+		}
+		public void Render() {
+			ConsoleColor prevColor = Console.ForegroundColor;
+
+			for (int y = 0; y < _size; y++) {
+				for (int x = 0; x < _size; x++) {
+					Console.ForegroundColor = GetTileColor(_tile[y,x]);
+					Console.Write(TILE + " ");
+				}
+				Console.WriteLine();
+			}
+
+			Console.ForegroundColor = prevColor;
+		}
+
+		ConsoleColor GetTileColor(TileType type) {
+			switch (type) {
+				case TileType.Empty:
+					return ConsoleColor.DarkGreen;
+				case TileType.Wall:
+					return ConsoleColor.Red;
+				default:
+					return ConsoleColor.Blue;
+			}
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace Pathfinder_Algorithm {
 	internal class Program {
 		static void Main(string[] args) {
+			Maze maze = new Maze();
+			maze.init(25);
+
 			Console.CursorVisible = false; // 콘솔 커서 표시 안함
 
 			const int WAIT_TICK = 1000 / 30;
-			const char WALL = '\u25a0';
 			int lastTick = 0;
 
 			while (true) {
@@ -21,14 +23,7 @@
 
 				// RENDERING
 				Console.SetCursorPosition(0, 0); // 콘솔 커서의 위치를 0, 0으로 고정
-
-				for (int i = 0; i < 25; i++) {
-					for (int j = 0; j < 25; j++) {
-						Console.ForegroundColor = ConsoleColor.DarkGreen;
-						Console.Write(WALL);
-					}
-                    Console.WriteLine();
-                }
+				maze.Render();
             }
 		}
 	}

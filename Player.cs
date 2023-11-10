6cb809f6fad88a96d9a946683000b8ede9e1af7d
sch990428 <990428sch@naver.com>
2023-11-10 17:34:44 +0900

    AStar 알고리즘을 이용한 길찾기

--- Player.cs
+++ Player.cs
@@ -24,8 +24,8 @@
 			_maze = maze;
 
 			//RightHand();
-
-			BFS();
+			//BFS();
+			AStar();
 		}
 
 		private void RightHand() {
@@ -91,10 +91,11 @@
 				}
 			}
 
+			CalcPathFromParent(parent);
+		}
+		void CalcPathFromParent(Pos[,] parent) {
 			int y = _maze.DestY;
 			int x = _maze.DestX;
-
-			Console.WriteLine(y + " " + x);
 			while (parent[y, x].Y != y || parent[y, x].X != x) {
 				_paths.Add(new Pos(y, x));
 				Pos pos = parent[y, x];
@@ -103,6 +104,79 @@
 			}
 			_paths.Add(new Pos(y, x));
 			_paths.Reverse();
+		}
+		struct PQNode : IComparable<PQNode> {
+			public int F;
+			public int G;
+			public int Y;
+			public int X;
+
+			public int CompareTo(PQNode other) {
+				if (F == other.F) { return 0; }
+				return F < other.F ? 1 : -1;
+			}
+		}
+		private void AStar() {
+			int[] deltaY = new int[] { -1, 0, 1, 0 };
+			int[] deltaX = new int[] { 0, -1, 0, 1 };
+			int[] cost = new int[] { 1, 1, 1, 1 };
+
+			//점수 도출식 : F = G + H
+			//F : 최종 점수, G : 시작점에서 해당 좌표까지 이동하는데 드는 비용, H : 목적지에서 얼마나 가까운지
+
+			bool[,] closed = new bool[_maze.Size, _maze.Size]; //해당 좌표를 이미 방문했는가
+			int[,] open = new int[_maze.Size, _maze.Size]; //경로를 한번이라도 발견했는가
+
+			for (int y = 0; y < _maze.Size; y++) {
+				for (int x = 0; x <  _maze.Size; x++) {
+					open[y, x] = Int32.MaxValue;
+				}
+			}
+
+			Pos[,] parent = new Pos[_maze.Size, _maze.Size]; 
+
+			PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>(); //open 리스트의 정보들 중 최적의 후보를 뽑기위한 우선순위 큐
+
+			//시작점 예약
+			open[PosY, PosX] = 0 + Math.Abs(_maze.DestY - PosY) + Math.Abs(_maze.DestX - PosX);
+			pq.Push(new PQNode() { F = Math.Abs(_maze.DestY - PosY) + Math.Abs(_maze.DestX - PosX), G = 0, Y = PosY, X = PosX});
+
+			parent[PosY, PosX] = new Pos(PosY, PosX);
+
+			while (pq.Count > 0) {
+				//최적의 후보 찾기
+				PQNode node = pq.Pop();
+				if (closed[node.Y, node.X]) { continue; }
+
+				closed[node.Y, node.X] = true; //방문
+
+				if (node.Y == _maze.DestY && node.X == _maze.DestX) { break; } //목적지 도달 시 종료
+
+				//이동할 수 있는 좌표인지 확인 후 예약
+				for (int i = 0; i < 4; i++) {
+					int nextY = node.Y + deltaY[i];
+					int nextX = node.X + deltaX[i];
+
+					//유효 조건에서 어긋나면 스킵
+					if (nextX < 0 || nextX >= _maze.Size || nextY < 0 || nextY >= _maze.Size) { continue; }
+					if (_maze.Tile[nextY, nextX] == Maze.TileType.Wall) { continue; }
+					if (closed[nextY, nextX]) { continue; }
+
+					//비용 계산
+					int g = node.G + cost[i];
+					int h = Math.Abs(_maze.DestY - nextY) + Math.Abs(_maze.DestX - nextX);
+
+					//더 빠른 길을 찾은 경우 스킵
+					if (open[nextY, nextX] < g + h) { continue; }
+
+					//예약
+					open[nextY, nextX] = g + h;
+					pq.Push(new PQNode() { F = g + h, G = g, Y = nextY, X = nextX });
+					parent[nextY, nextX] = new Pos(node.Y, node.X);
+				}
+			}
+
+			CalcPathFromParent(parent);
 		}
 
 		const int MOVE_TICK = 10;
---
+++ PriorityQueue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pathfinder_Algorithm {
+	class PriorityQueue<T> where T : IComparable<T> {
+		List<T> _heap = new List<T>();
+		public T Pop() {
+			//반환값을 따로 저장
+			T ret = _heap[0];
+
+			//마지막 데이터를 루트로 이동
+			int lastIndex = _heap.Count - 1;
+			_heap[0] = _heap[lastIndex];
+			_heap.RemoveAt(lastIndex);
+			lastIndex--;
+
+			int now = 0;
+			while (true) {
+				int left = 2 * now + 1;
+				int right = 2 * now + 2;
+
+				int next = now;
+				//왼쪽 값이 현재값보다 크면 왼쪽으로 이동
+				if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0) { next = left; }
+				//오른쪽 값이 현재값보다 크면 오른쪽으로 이동
+				if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0) { next = right; }
+				//왼쪽 오른쪽 모두 현재 값보다 작으면 종료
+				if (next == now) { break; }
+
+				//두 값을 스왑
+				T temp = _heap[now];
+				_heap[now] = _heap[next];
+				_heap[next] = temp;
+
+				now = next;
+			}
+
+			return ret;
+		}
+		public void Push(T n) {
+			// 힙의 맨 끝에 새로운 데이터 삽입
+			_heap.Add(n);
+
+			int now = _heap.Count - 1;
+
+			//부모 노드와 비교해가며 스왑
+			while (now > 0) {
+				int next = (now - 1) / 2;
+				if (_heap[now].CompareTo(_heap[next]) < 0) { break; }
+
+				T temp = _heap[now];
+				_heap[now] = _heap[next];
+				_heap[next] = temp;
+
+				now = next;
+			}
+		}
+		public int Count { get { return _heap.Count; } }
+	}
+}
